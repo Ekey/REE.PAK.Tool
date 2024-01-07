c487698611b5387f66945d1cf4aef77a21ea2c29
Ekey <infinirt@hotmail.com>
2024-01-07 17:40:05 +0300

    Main

--- REE.Unpacker/REE.Unpacker/Program.cs
+++ REE.Unpacker/REE.Unpacker/Program.cs
@@ -15,17 +15,18 @@ namespace REE.Unpacker
             Console.WriteLine("(c) 2023 Ekey (h4x0r) / v{0}\n", Utils.iGetApplicationVersion());
             Console.ResetColor();
 
-            if (args.Length != 3)
+            if (args.Length != 2 && args.Length != 3)
             {
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("[Usage]");
                 Console.WriteLine("    REE.Unpacker <m_ProjectFile> <m_File> <m_Directory>\n");
                 Console.WriteLine("    m_ProjectFile - Project file (Tag) with filenames (file must be in Projects folder)");
                 Console.WriteLine("    m_File - Source of PAK archive file");
-                Console.WriteLine("    m_Directory - Destination directory\n");
+                Console.WriteLine("    m_Directory - Destination directory (Optional)\n");
                 Console.ResetColor();
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("[Examples]");
+                Console.WriteLine("    REE.Unpacker MHR_PC_DEMO E:\\Games\\MHR\\re_chunk_000.pak");
                 Console.WriteLine("    REE.Unpacker MHR_PC_DEMO E:\\Games\\MHR\\re_chunk_000.pak D:\\Unpacked");
                 Console.ResetColor();
                 return;
@@ -33,7 +34,16 @@ namespace REE.Unpacker
 
             String m_ListFile = args[0];
             String m_PakFile = args[1];
-            String m_Output = Utils.iCheckArgumentsPath(args[2]);
+            String m_Output = null;
+
+            if (args.Length == 2)
+            {
+                m_Output = Path.GetDirectoryName(args[1]) + @"\" + Path.GetFileNameWithoutExtension(args[1]) + @"\";
+            }
+            else
+            {
+                m_Output = Utils.iCheckArgumentsPath(args[2]);
+            }
 
             if (!File.Exists("Zstandard.Net.dll") || !File.Exists("libzstd.dll"))
             {
