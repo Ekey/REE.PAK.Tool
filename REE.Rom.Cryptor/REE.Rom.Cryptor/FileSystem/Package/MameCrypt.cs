bce8106df072571c5396ed0b7238fb1457fa7102
Ekey <infinirt@hotmail.com>
2024-12-05 22:09:54 +0300

    Main

--- REE.Rom.Cryptor/REE.Rom.Cryptor/FileSystem/Package/MameCrypt.cs
+++ REE.Rom.Cryptor/REE.Rom.Cryptor/FileSystem/Package/MameCrypt.cs
@@ -7,7 +7,17 @@ namespace REE.Rom.Cryptor
     {
         public static void iDoIt(String m_SrcFile, String m_DstFile)
         {
-            String m_NativePath = "natives/STM/streaming/Roms/" + Path.GetFileName(m_SrcFile);
+            String m_NativePath = String.Empty;
+            String m_FileName = Path.GetFileName(m_SrcFile);
+
+            if (!m_FileName.Contains("_d."))
+            {
+                m_NativePath = "natives/STM/streaming/Roms/" + Path.GetFileName(m_SrcFile);
+            }
+            else
+            {
+                m_NativePath = "natives/STM/streaming/Roms/DecryptionRom/" + Path.GetFileName(m_SrcFile).Replace("_d", "_D");
+            }
 
             var lpBuffer = File.ReadAllBytes(m_SrcFile);
 
